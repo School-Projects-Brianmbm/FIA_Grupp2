96094e81ac473d57cf196d1aa3758349ad829fd6
Christian Karlsson <battousai877@gmail.com>
2024-02-28 12:57:15 +0100

    midwork commit, unblocking nav forth to in-game view

--- LobbyPage.xaml.cs
+++ LobbyPage.xaml.cs
@@ -20,11 +20,27 @@ namespace FIA_Grupp2
 	/// <summary>
 	/// An empty page that can be used on its own or navigated to within a Frame.
 	/// </summary>
+	/// 
+
+	public enum SlotChoice
+	{
+		Player,
+		AI,
+		None
+	}
+
 	public sealed partial class LobbyPage : Page
 	{
 		public LobbyPage()
 		{
 			this.InitializeComponent();
+
+			SlotChoice slot1 = SlotChoice.Player;
+			SlotChoice slot2 = SlotChoice.Player;
+			SlotChoice slot3 = SlotChoice.None;
+			SlotChoice slot4 = SlotChoice.None;
+
+			txtblock.Text = "Pla";
 		}
 
 		private void Click_Choose_level(object sender, RoutedEventArgs e)
@@ -32,5 +48,70 @@ namespace FIA_Grupp2
 
         }
 
+		private void Slot_1_user(object sender, RoutedEventArgs e)
+		{
+
+			Button button = sender as Button;
+			if (button != null && button.Content != null)
+			{
+				string selectedChoice = button.Content.ToString();
+
+				switch (selectedChoice)
+				{
+					case "Player":
+
+						button.Content = "AI";
+						txtblock.Text = "AI";
+						break;
+
+					case "AI":
+
+						button.Content = "None";
+						txtblock.Text = "---";
+						break;
+
+					case "None":
+
+						button.Content = "Player";
+						txtblock.Text = "Player1";
+						break;
+
+					default:
+						break;
+				}
+			}
+		}
+
+		private void ComboBox_slot1User_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			
+			ComboBox comboBox = sender as ComboBox;
+			if (comboBox != null && comboBox.SelectedItem != null)
+			{
+				string selectedChoice = (comboBox.SelectedItem as ComboBoxItem).Content.ToString();
+				//txtblock.Text = "hej";
+
+				switch (selectedChoice)
+				{
+					case "Player":
+
+						//txtblock.Text = "player!!";
+						break;
+
+					case "AI":
+						
+						
+						break;
+
+					case "None":
+						
+						
+						break;
+
+					default:
+						break;
+				}
+			}
+		}
 	}
 }
