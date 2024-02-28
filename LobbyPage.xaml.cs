580124ac433bbeb219c684ccd8a352cbe3863827
Christian Karlsson <battousai877@gmail.com>
2024-02-28 15:09:04 +0100

    added initial logic for userbuttons and teambuttons

--- LobbyPage.xaml.cs
+++ LobbyPage.xaml.cs
@@ -1,27 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+using Windows.UI.Xaml.Media.Imaging;
 
 namespace FIA_Grupp2
-{
-	/// <summary>
-	/// An empty page that can be used on its own or navigated to within a Frame.
-	/// </summary>
-	/// 
-
+{	
+	//Could be used for 
 	public enum SlotChoice
 	{
 		Player,
@@ -31,24 +15,154 @@ namespace FIA_Grupp2
 
 	public sealed partial class LobbyPage : Page
 	{
+		//Used to store choosable teamicons
+		private BitmapImage[] teamicons;
+
+		//Index to keep track of choosen teamicon
+		private int slot1IconIndex;
+		private int slot2IconIndex;
+		private int slot3IconIndex;
+		private int slot4IconIndex;
+
 		public LobbyPage()
 		{
 			this.InitializeComponent();
 
-			SlotChoice slot1 = SlotChoice.Player;
-			SlotChoice slot2 = SlotChoice.Player;
-			SlotChoice slot3 = SlotChoice.None;
-			SlotChoice slot4 = SlotChoice.None;
+			// Initialize the array of BitmapImages
+			teamicons = new BitmapImage[]
+			{
+				new BitmapImage(new Uri("ms-appx:///Assets/TeamIcons/cow.jpg")),
+				new BitmapImage(new Uri("ms-appx:///Assets/TeamIcons/pig.jpg")),
+				new BitmapImage(new Uri("ms-appx:///Assets/TeamIcons/chicken.jpg")),
+				new BitmapImage(new Uri("ms-appx:///Assets/TeamIcons/sheep.jpg"))
+			};
 
-			txtblock.Text = "Pla";
+			//Starting iconindex dor each slot
+			slot1IconIndex = 0;
+			slot2IconIndex = 1;
+			slot3IconIndex = 2;
+			slot4IconIndex = 3;
+
 		}
 
-		private void Click_Choose_level(object sender, RoutedEventArgs e)
+		private void slot1_user_selection_button_click_event(object sender, RoutedEventArgs e)
 		{
+			Button button = sender as Button;
+			if (button != null && button.Content != null)
+			{
+				string selectedChoice = button.Content.ToString();
 
-        }
+				switch (selectedChoice)
+				{
+					case "Player":
 
-		private void Slot_1_user(object sender, RoutedEventArgs e)
+						button.Content = "AI";
+						slot1_username.Text = "AI";
+						slot1_button_teamicon.IsEnabled = true;
+						slot1_rectangle.Opacity = 1;
+						break;
+
+					case "AI":
+
+						button.Content = "None";
+						slot1_username.Text = "---";
+						slot1_button_teamicon.IsEnabled = false;
+						slot1_rectangle.Opacity = 0.5;
+						break;
+
+					case "None":
+
+						button.Content = "Player";
+						slot1_username.Text = "Player1";
+						slot1_button_teamicon.IsEnabled = true;
+						slot1_rectangle.Opacity = 1;
+						break;
+
+					default:
+						break;
+				}
+			}
+		}
+
+		private void slot2_user_selection_button_click_event(object sender, RoutedEventArgs e)
+		{
+			Button button = sender as Button;
+			if (button != null && button.Content != null)
+			{
+				string selectedChoice = button.Content.ToString();
+
+				switch (selectedChoice)
+				{
+					case "Player":
+
+						button.Content = "AI";
+						slot2_username.Text = "AI";
+						slot2_button_teamicon.IsEnabled = true;
+						slot2_rectangle.Opacity = 1;
+						break;
+
+					case "AI":
+
+						button.Content = "None";
+						slot2_username.Text = "---";
+						slot2_button_teamicon.IsEnabled = false;
+						slot2_rectangle.Opacity = 0.5;
+						break;
+
+					case "None":
+
+						button.Content = "Player";
+						slot2_username.Text = "Player2";
+						slot2_button_teamicon.IsEnabled = true;
+						slot2_rectangle.Opacity = 1;
+						break;
+
+					default:
+						break;
+				}
+			}
+		}
+
+		private void slot3_user_selection_button_click_event(object sender, RoutedEventArgs e)
+		{
+			Button button = sender as Button;
+			if (button != null && button.Content != null)
+			{
+				string selectedChoice = button.Content.ToString();
+
+				switch (selectedChoice)
+				{
+					case "Player":
+
+						button.Content = "AI";
+						slot3_username.Text = "AI";
+						slot3_button_teamicon.IsEnabled = true;
+						slot3_rectangle.Opacity = 1;
+						break;
+
+					case "AI":
+
+						button.Content = "None";
+						slot3_username.Text = "---";
+						slot3_button_teamicon.IsEnabled = false;
+						slot3_rectangle.Opacity = 0.5;
+						break;
+
+					case "None":
+
+						button.Content = "Player";
+						slot3_username.Text = "Player3";
+						slot3_button_teamicon.IsEnabled = true;
+						slot3_rectangle.Opacity = 1;
+						break;
+
+					default:
+						break;
+				}
+			}
+		}
+
+		private void slot4_user_selection_button_click_event(object sender, RoutedEventArgs e)
 		{
 
 			Button button = sender as Button;
@@ -61,19 +175,25 @@ namespace FIA_Grupp2
 					case "Player":
 
 						button.Content = "AI";
-						txtblock.Text = "AI";
+						slot4_username.Text = "AI";
+						slot4_button_teamicon.IsEnabled = true;
+						slot4_rectangle.Opacity = 1;
 						break;
 
 					case "AI":
 
 						button.Content = "None";
-						txtblock.Text = "---";
+						slot4_username.Text = "---";
+						slot4_button_teamicon.IsEnabled = false;
+						slot4_rectangle.Opacity = 0.5;
 						break;
 
 					case "None":
 
 						button.Content = "Player";
-						txtblock.Text = "Player1";
+						slot4_username.Text = "Player4";
+						slot4_button_teamicon.IsEnabled = true;
+						slot4_rectangle.Opacity = 1;
 						break;
 
 					default:
@@ -82,36 +202,62 @@ namespace FIA_Grupp2
 			}
 		}
 
-		private void ComboBox_slot1User_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		private void slot1_button_teamicon_click_event(object sender, RoutedEventArgs e)
 		{
-			
-			ComboBox comboBox = sender as ComboBox;
-			if (comboBox != null && comboBox.SelectedItem != null)
+			Button button = sender as Button;
+			if (button != null)
 			{
-				string selectedChoice = (comboBox.SelectedItem as ComboBoxItem).Content.ToString();
-				//txtblock.Text = "hej";
-
-				switch (selectedChoice)
-				{
-					case "Player":
-
-						//txtblock.Text = "player!!";
-						break;
-
-					case "AI":
-						
-						
-						break;
-
-					case "None":
-						
-						
-						break;
-
-					default:
-						break;
-				}
+				//Goes through the icons in teamicon array. When slot1IconIndex == teamicons.Length the remainder will be 0, reseting the slot1IconIndex back to 0.
+				slot1IconIndex = (slot1IconIndex + 1) % teamicons.Length;
+				slot1_teamicon.Source = teamicons[slot1IconIndex];
 			}
+		}
+
+		private void slot2_button_teamicon_click_event(object sender, RoutedEventArgs e)
+		{
+			Button button = sender as Button;
+			if (button != null)
+			{
+				slot2IconIndex = (slot2IconIndex + 1) % teamicons.Length;
+				slot2_teamicon.Source = teamicons[slot2IconIndex];
+			}
+		}
+
+		private void slot3_button_teamicon_click_event(object sender, RoutedEventArgs e)
+		{
+			Button button = sender as Button;
+			if (button != null)
+			{
+				slot3IconIndex = (slot3IconIndex + 1) % teamicons.Length;
+				slot3_teamicon.Source = teamicons[slot3IconIndex];
+			}
+		}
+
+		private void slot4_button_teamicon_click_event(object sender, RoutedEventArgs e)
+		{
+			Button button = sender as Button;
+			if (button != null)
+			{
+				slot4IconIndex = (slot4IconIndex + 1) % teamicons.Length;
+				slot4_teamicon.Source = teamicons[slot4IconIndex];
+			}
+		}
+
+		
+		
+		private void back_button_click_event(object sender, RoutedEventArgs e)
+		{
+			//TODO not yet implemented
+		}
+
+		private void options_button_click_event(object sender, RoutedEventArgs e)
+		{
+			//TODO not yet implemented
+		}
+
+		private void choose_level_button_click_event(object sender, RoutedEventArgs e)
+		{
+			//TODO not yet implemented
 		}
 	}
 }
