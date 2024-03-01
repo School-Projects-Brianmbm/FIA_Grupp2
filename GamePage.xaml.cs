f6a89959a33cf63da823aa2e7fe02029032a1668
Alex Wigforss <alex.wigforss@gmail.com>
2024-03-01 18:19:48 +0100

    globalCoarse (the path cows walking) plus two cows for testing

--- GamePage.xaml.cs
+++ GamePage.xaml.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Diagnostics;
+using Windows.UI.Composition;
+using Windows.UI;
 using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media.Imaging;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -12,13 +15,32 @@ namespace FIA_Grupp2
     public sealed partial class GamePage : Page
     {
         int MouseX, MouseY;
-        GameBoardGrid gameGrid;
+
+        static GameBoardGrid gameGrid;
+
+        Position goalPosition = new Position(5, 5);
+
+        static Position[] globalCoarse = new Position[44]
+        {
+            new Position(10, 6),new Position(9, 6),new Position(8, 6),new Position(7, 6),new Position(6, 6),new Position(6, 7),new Position(6, 8),new Position(6, 9),
+            new Position(6, 10),new Position(5, 10),new Position(4, 10),new Position(4, 9),new Position(4, 8),new Position(4, 7),new Position(4, 6),new Position(3, 6),
+            new Position(2, 6),new Position(1, 6),new Position(0, 6),new Position(0, 5),new Position(0, 4),new Position(1, 4),new Position(2, 4),new Position(3, 4),
+            new Position(4, 4),new Position(4, 3),new Position(4, 2),new Position(4, 1),new Position(4, 0),new Position(5, 0),new Position(6, 0),new Position(6, 1),
+            new Position(6, 2),new Position(6, 3),new Position(6, 4),new Position(7, 4),new Position(8, 4),new Position(9, 4),new Position(10, 4),new Position(10, 5),
+            new Position(9, 5),new Position(8, 5),new Position(7, 5),new Position(6, 5)
+        };
+
+        Team cows, team2;
+        // Pawn pawn = new Pawn();
+
+
 
         public GamePage()
         {
             InitializeComponent();
             Window.Current.CoreWindow.PointerMoved += CoreWindow_PointerMoved;
             layoutRoot.PointerWheelChanged += new PointerEventHandler(PointerWheelChanged);
+            // layoutRoot.PointerReleased += new PointerEventHandler(PointerReleased);
             Loaded += MainPage_Loaded;
         }
 
@@ -34,10 +56,16 @@ namespace FIA_Grupp2
             gameGrid.CalculateOrigoY();
             gameGrid.SetEllipsesPositions();
 
-            Debug.Write(gameGrid.GetActualPositionOf(10, 10) + "\n");
+            cows = new Team(gameGrid, globalCoarse, new Position(9, 9));
+            team2 = new Team(gameGrid, globalCoarse, new Position(1, 1));
+
+            layoutRoot.Children.Add(cows.Pawn.PawnCanvas);
+            layoutRoot.Children.Add(team2.Pawn.PawnCanvas);
+
+
+            Debug.Write("Length of coarse is: " + gameGrid.CountCourseLength());
+            //Debug.Write(gameGrid.GetActualPositionOf(10, 10) + "\n");
         }
-
-
 
         private void CoreWindow_PointerMoved(CoreWindow sender, PointerEventArgs args)
         {
@@ -56,11 +84,16 @@ namespace FIA_Grupp2
             MouseY = roundedY;
         }
 
+
         private void DebugTextUpdateModifier()
         {
             debugtext.Text = $"Mouse Position: X={MouseX}, Y={MouseY}, {gameGrid.Squish}";
         }
 
+        private new void PointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+            cows.Pawn.Step();
+        }
 
         private new void PointerWheelChanged(object sender, PointerRoutedEventArgs e)
         {
@@ -88,7 +121,7 @@ namespace FIA_Grupp2
 
             DebugTextUpdateModifier();
 
-            Debug.Write(gameGrid.GetActualPositionOf(10, 10) + "\n");
+            //Debug.Write(gameGrid.GetActualPositionOf(10, 10) + "\n");
         }
     }
 }
