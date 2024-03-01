6d07fc5b15105d9c728603c00e72a2af7b4dcd54
Alex Wigforss <alex.wigforss@gmail.com>
2024-03-01 18:23:30 +0100

    Team and Pawn

---
+++ Pawn.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Diagnostics;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace FIA_Grupp2
+{
+    internal class Pawn
+    {
+        // GameBoardGrid board;
+        Position indexPosition;
+        GameBoardGrid boardgrid;
+        int steps = 0;
+        public Position[] coarse;
+
+        Canvas pawnCanvas = new Canvas
+        {
+            Name = "pawn_1_canvas",
+            Height = 80,
+            Width = 80
+        };
+
+        Image pawnImage = new Image
+        {
+            Name = "team_1_pawn_1",
+            Source = new BitmapImage(new Uri("ms-appx:///Assets/Pawns/cow_0.png")),
+            Height = 80,
+            Width = 80,
+        };
+
+        public Canvas PawnCanvas { get => pawnCanvas; set => pawnCanvas = value; }
+        public Image PawnImage { get => pawnImage; set => pawnImage = value; }
+
+        public Pawn(GameBoardGrid gbg, Position startpos,ref Position[] teamcoarse)
+        {
+            pawnCanvas.Children.Add(pawnImage);
+            indexPosition = startpos;
+            boardgrid = gbg;
+            PositionAtNest();
+            coarse = teamcoarse;
+        }
+
+        void PositionAtNest()
+        {
+            Point pos = boardgrid.GetActualPositionOf(indexPosition.X, indexPosition.Y);
+            // Debug.WriteLine(pos.ToString());
+            double newX = pos.X;
+            double newY = pos.Y;
+            Canvas.SetLeft(PawnImage, newX + 8);
+            Canvas.SetTop(PawnImage, newY - 10);
+        }
+
+        void PositionAt(Position ind)
+        {
+            Point pos = boardgrid.GetActualPositionOf(ind.X, ind.Y);
+            // Debug.WriteLine(pos.ToString());
+            double newX = pos.X;
+            double newY = pos.Y;
+            Canvas.SetLeft(PawnImage, newX + 8);
+            Canvas.SetTop(PawnImage, newY - 10);
+        }
+
+        internal void Step()
+        {
+            PositionAt(coarse[steps++]);
+            Debug.Write($"OneStep: {steps} \n");
+        }
+    }
+}
---
+++ Team.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIA_Grupp2
+{
+    internal class Team
+    {
+        static Position nestIndex;
+        public Position[] coarse;
+        GameBoardGrid boardgrid;
+        // Pawn[] pawns = new Pawn[4];
+
+        Pawn pawn;
+        internal Pawn Pawn { get => pawn; set => pawn = value; }
+
+        public Team(GameBoardGrid gbg, Position[] coarse, Position start)
+        {
+            this.coarse = coarse;
+            nestIndex = start;
+            boardgrid = gbg;
+
+            pawn = new Pawn(boardgrid, nestIndex, ref this.coarse);
+        }
+    }
+}
