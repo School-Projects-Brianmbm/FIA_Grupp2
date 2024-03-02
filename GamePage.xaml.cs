9227599e529b3b61bef895578a3099f9b8a0fe20
Alex Wigforss <alex.wigforss@gmail.com>
2024-03-02 17:26:03 +0100

    added goal position to globalCoarse

--- GamePage.xaml.cs
+++ GamePage.xaml.cs
@@ -20,14 +20,14 @@ namespace FIA_Grupp2
 
         Position goalPosition = new Position(5, 5);
 
-        static Position[] globalCoarse = new Position[44]
+        static Position[] globalCoarse = new Position[45]
         {
             new Position(10, 6),new Position(9, 6),new Position(8, 6),new Position(7, 6),new Position(6, 6),new Position(6, 7),new Position(6, 8),new Position(6, 9),
             new Position(6, 10),new Position(5, 10),new Position(4, 10),new Position(4, 9),new Position(4, 8),new Position(4, 7),new Position(4, 6),new Position(3, 6),
             new Position(2, 6),new Position(1, 6),new Position(0, 6),new Position(0, 5),new Position(0, 4),new Position(1, 4),new Position(2, 4),new Position(3, 4),
             new Position(4, 4),new Position(4, 3),new Position(4, 2),new Position(4, 1),new Position(4, 0),new Position(5, 0),new Position(6, 0),new Position(6, 1),
             new Position(6, 2),new Position(6, 3),new Position(6, 4),new Position(7, 4),new Position(8, 4),new Position(9, 4),new Position(10, 4),new Position(10, 5),
-            new Position(9, 5),new Position(8, 5),new Position(7, 5),new Position(6, 5)
+            new Position(9, 5),new Position(8, 5),new Position(7, 5),new Position(6, 5), new Position(5, 5)
         };
 
         Team cows, team2;
@@ -60,7 +60,7 @@ namespace FIA_Grupp2
 
             gameGrid.CalculateActualPositions();
             gameGrid.CalculateOrigoY();
-            gameGrid.SetEllipsesPositions();
+            gameGrid.SetEllipsesPositions(true,showInd: true);
 
             cows = new Team(gameGrid, globalCoarse, new Position(9, 9));
             team2 = new Team(gameGrid, globalCoarse, new Position(1, 1));
