e6de8a85e4ca812b52d25f5618d5c68742ef20b9
Christian Karlsson <battousai877@gmail.com>
2024-03-03 16:21:19 +0100

    Basic logic for game- and turntimer

--- GamePage.xaml.cs
+++ GamePage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Diagnostics;
+using Windows.Storage;
 using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -16,13 +18,44 @@ namespace FIA_Grupp2
 
         private Dice _dice;
 
-        public GamePage()
+		private DispatcherTimer gameTimer;
+		private DispatcherTimer turnTimer;
+		private TimeSpan remainingGameTime;
+		private TimeSpan remainingTurnTime;
+
+		public GamePage()
         {
             InitializeComponent();
             Window.Current.CoreWindow.PointerMoved += CoreWindow_PointerMoved;
             layoutRoot.PointerWheelChanged += new PointerEventHandler(PointerWheelChanged);
             Loaded += MainPage_Loaded;
-        }
+
+            //Get saved game session options data
+			string serializedData = (string)ApplicationData.Current.LocalSettings.Values["SessionOptionsData"];
+			if (!string.IsNullOrEmpty(serializedData))
+			{
+				GameSessionOptions sessionOptionsData = JsonConvert.DeserializeObject<GameSessionOptions>(serializedData);
+				int gameHours = int.Parse(sessionOptionsData.GameTimeHours.ToString());
+				int gameMinutes = int.Parse(sessionOptionsData.GameTimeMinutes.ToString());
+				int gameSeconds = int.Parse(sessionOptionsData.GameTimeSeconds.ToString());
+				int turnHours = int.Parse(sessionOptionsData.TurnTimeHours.ToString());
+				int turnMinutes = int.Parse(sessionOptionsData.TurnTimeMinutes.ToString());
+				int turnSeconds = int.Parse(sessionOptionsData.TurnTimeSeconds.ToString());
+
+				remainingGameTime = new TimeSpan(gameHours, gameMinutes, gameSeconds);
+				remainingTurnTime = new TimeSpan(turnHours, turnMinutes, turnSeconds);
+
+				gameTimer = new DispatcherTimer();
+				gameTimer.Interval = TimeSpan.FromSeconds(1);
+				gameTimer.Tick += GameTimerTick;
+				gameTimer.Start();
+
+				turnTimer = new DispatcherTimer();
+				turnTimer.Interval = TimeSpan.FromSeconds(1);
+				turnTimer.Tick += TurnTimerTick;
+				turnTimer.Start();
+			}
+		}
 
         private void DiceClicked(object sender, RoutedEventArgs e)
         {
@@ -98,5 +131,42 @@ namespace FIA_Grupp2
 
             Debug.Write(gameGrid.GetActualPositionOf(10, 10) + "\n");
         }
-    }
+
+		private void GameTimerTick(object sender, object e)
+		{
+			if (remainingGameTime.TotalSeconds > 0)
+			{
+				remainingGameTime = remainingGameTime.Subtract(TimeSpan.FromSeconds(1));
+				UpdateGameTimerText();
+			}
+			else
+			{
+				gameTimer.Stop();
+				// TODO Stop the game as the gametimer has run out.
+			}
+		}
+
+		private void TurnTimerTick(object sender, object e)
+		{
+			if (remainingTurnTime.TotalSeconds > 0)
+			{
+				remainingTurnTime = remainingTurnTime.Subtract(TimeSpan.FromSeconds(1));
+				UpdateTurnTimerText();
+			}
+			else
+			{
+				gameTimer.Stop();
+				// TODO Stop the game as the gametimer has run out.
+			}
+		}
+
+		private void UpdateGameTimerText()
+		{
+			gameTimerText.Text = $"{remainingGameTime.Hours:D2}:{remainingGameTime.Minutes:D2}:{remainingGameTime.Seconds:D2}";
+		}
+		private void UpdateTurnTimerText()
+		{
+			turnTimerText.Text = $"{remainingTurnTime.Hours:D2}:{remainingTurnTime.Minutes:D2}:{remainingTurnTime.Seconds:D2}";
+		}
+	}
 }
