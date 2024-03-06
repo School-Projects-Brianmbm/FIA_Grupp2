c926acfddea758e8f42d425b8358ecb7c5f3977d
Alex Wigforss <alex.wigforss@gmail.com>
2024-03-06 14:29:32 +0100

    made canvases clickable (defaulted em to IsHitTestVisible = false)

--- Pawn.cs
+++ Pawn.cs
@@ -1,38 +1,66 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Xml.Linq;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media.Imaging;
 
 namespace FIA_Grupp2
 {
     internal class Pawn
     {
+        protected bool canWalk = false;
+        protected static int GLOBAL_INDEX = 0;
+        protected int globalIndex;
+        protected int localIndex;
+        protected string Name = "Pawn";
         protected bool isInGoal = false;
         protected int direction;
         protected Position indexPosition;
         protected GameBoardGrid boardgrid;
         protected int steps = 0;
+        private int turnStepsLeft = 0;
+        internal int TurnStepsLeft { get => turnStepsLeft; set => turnStepsLeft = value; }
+
         public Position[] coarse;
 
         protected Canvas pawnCanvas = new Canvas { Height = 80, Width = 80 };
         protected Image pawnImage = new Image();
 
         internal Image[] pawnImages = new Image[4];
+
         public Canvas PawnCanvas { get => pawnCanvas; set => pawnCanvas = value; }
         public Image PawnImage { get => pawnImage; set => pawnImage = value; }
+
 
         public bool AtNest
         {
             get { return steps == 0; }
         }
 
+
         public Pawn(GameBoardGrid gbg, Position startpos, ref Position[] teamcoarse)
         {
+            PawnCanvas.IsHitTestVisible = false;
+            Debug.Write(Team.NUMBER_OF_TEAMS);
+            globalIndex = GLOBAL_INDEX++;
             pawnCanvas.Children.Add(pawnImage);
             indexPosition = startpos;
             boardgrid = gbg;
             PositionAtNest();
             coarse = teamcoarse;
+            pawnCanvas.PointerPressed += Canvas_PointerPressed;
+        }
+
+        private void Canvas_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            if (e.OriginalSource is UIElement clickedElement)
+            {
+                Debug.Write($"{globalIndex} {localIndex} {Name} is clicked And canWalk = {canWalk}\n");
+                Step();
+                pawnCanvas.IsHitTestVisible = false;
+            }
         }
 
         public virtual void PositionAtNest()
@@ -53,33 +81,37 @@ namespace FIA_Grupp2
         }
         internal virtual void Step()
         {
-            if (isInGoal)
+            while (turnStepsLeft-- > 0)
             {
-                Debug.Write($"Im home already :)");
-            }
-            else if (steps == coarse.Length - 1)
-            {
-                PositionAt(coarse[steps++]);
-                isInGoal = true;
-            }
-            else
-            {
-                // Debug.Write($"\n");
-                // Debug.Write($"Direction: {direction} ");
-                // Debug.Write($" From: {coarse[steps]} ");
-                // Debug.Write($" To: {coarse[steps + 1]} ");
-                if (!IsSameDirection())
+
+                if (isInGoal)
                 {
-                    if (direction == 2 && coarse[steps + 1].X < coarse[steps].X) { TurnImageLeft(); }
-                    else if (direction == 2 && coarse[steps + 1].X > coarse[steps].X) { TurnImageRight(); }
-                    else if (direction == 3 && coarse[steps + 1].Y < coarse[steps].Y) { TurnImageLeft(); }
-                    else if (direction == 3 && coarse[steps + 1].Y > coarse[steps].Y) { TurnImageRight(); }
-                    else if (direction == 0 && coarse[steps + 1].X < coarse[steps].X) { TurnImageRight(); }
-                    else if (direction == 0 && coarse[steps + 1].X > coarse[steps].X) { TurnImageLeft(); }
-                    else if (direction == 1 && coarse[steps + 1].Y < coarse[steps].Y) { TurnImageRight(); }
-                    else if (direction == 1 && coarse[steps + 1].Y > coarse[steps].Y) { TurnImageLeft(); }
+                    Debug.Write($"Im home already :)");
                 }
-                PositionAt(coarse[steps++]);
+                else if (steps == coarse.Length - 1)
+                {
+                    PositionAt(coarse[steps++]);
+                    isInGoal = true;
+                }
+                else
+                {
+                    // Debug.Write($"\n");
+                    // Debug.Write($"Direction: {direction} ");
+                    // Debug.Write($" From: {coarse[steps]} ");
+                    // Debug.Write($" To: {coarse[steps + 1]} ");
+                    if (!IsSameDirection())
+                    {
+                        if (direction == 2 && coarse[steps + 1].X < coarse[steps].X) { TurnImageLeft(); }
+                        else if (direction == 2 && coarse[steps + 1].X > coarse[steps].X) { TurnImageRight(); }
+                        else if (direction == 3 && coarse[steps + 1].Y < coarse[steps].Y) { TurnImageLeft(); }
+                        else if (direction == 3 && coarse[steps + 1].Y > coarse[steps].Y) { TurnImageRight(); }
+                        else if (direction == 0 && coarse[steps + 1].X < coarse[steps].X) { TurnImageRight(); }
+                        else if (direction == 0 && coarse[steps + 1].X > coarse[steps].X) { TurnImageLeft(); }
+                        else if (direction == 1 && coarse[steps + 1].Y < coarse[steps].Y) { TurnImageRight(); }
+                        else if (direction == 1 && coarse[steps + 1].Y > coarse[steps].Y) { TurnImageLeft(); }
+                    }
+                    PositionAt(coarse[steps++]);
+                }
             }
         }
         internal virtual bool IsSameDirection()
@@ -124,6 +156,8 @@ namespace FIA_Grupp2
         public Cow(GameBoardGrid gbg, Position startpos, ref Position[] teamcoarse)
             : base(gbg, startpos, ref teamcoarse)
         {
+            Name = "Cow";
+            pawnCanvas.Name = "Cow";
             direction = 3;
             pawnImages = new Image[4]
             {
@@ -142,6 +176,10 @@ namespace FIA_Grupp2
         public Hen(GameBoardGrid gbg, Position startpos, ref Position[] teamcoarse)
             : base(gbg, startpos, ref teamcoarse)
         {
+            localIndex = globalIndex % 4;
+
+            Name = "Hen";
+            pawnCanvas.Name = "Hen";
             direction = 0;
             pawnImages = new Image[4]
             {
@@ -160,6 +198,10 @@ namespace FIA_Grupp2
         public Sheep(GameBoardGrid gbg, Position startpos, ref Position[] teamcoarse)
             : base(gbg, startpos, ref teamcoarse)
         {
+            localIndex = globalIndex % 4;
+
+            Name = "Sheep";
+            pawnCanvas.Name = "Sheep";
             direction = 1;
             pawnImages = new Image[4]
             {
@@ -178,6 +220,10 @@ namespace FIA_Grupp2
         public Pig(GameBoardGrid gbg, Position startpos, ref Position[] teamcoarse)
             : base(gbg, startpos, ref teamcoarse)
         {
+            localIndex = globalIndex % 4;
+
+            Name = "Pig";
+            pawnCanvas.Name = "Pig";
             direction = 2;
             pawnImages = new Image[4]
             {
